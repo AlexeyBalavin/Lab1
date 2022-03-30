0ae365a61bb893ba546eaa5a45df54761b946fef
3kosu <54220951+3kosu@users.noreply.github.com>
2022-03-30 22:43:21 +0500

    1

--- krug.py
+++ krug.py
@@ -1,18 +1,18 @@
 from PIL import Image
-
 img = Image.open("./images/white.jpg")
 width, height = img.size
 
 
 for y in range(height):
     for x in range(width):
-        dist = min(abs(x - width), x, abs(y - height), y) # 
+        dist = ((x - width/2) ** 2 + (y - height/2) ** 2) ** (1/2)
 
-        dist = dist / (height / 2) # соотношение от центра к краю
+        
+        dist = dist / ((2**(1/2) * (height / 2))) # соотношение от центра к краю
 
-        r = 90 * dist + 0 *(1 - dist)
-        g = 0 * dist + 180 * (1 - dist)
-        b = 0 * dist + 25 * (1 - dist)
+        r = 0 * dist + 150 *(1 - dist)
+        g = 30 * dist + 80 * (1 - dist)
+        b = 0 * dist + 125 * (1 - dist)
         img.putpixel((x, y), (int(r), int(g), int(b)))
 
 img.save('./images/gradient.jpg')
